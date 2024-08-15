d3c9a124f7188323c07b7ece5ec667d79823b0ac
gmattedi7 <giuliamema@hotmail.com>
2024-08-15 09:50:57 -0300

    Update Dom_GGMS_Model.cpp

--- Dom_GGMS_Model.cpp
+++ Dom_GGMS_Model.cpp
@@ -1,3 +1,10 @@
+//DOM-GGMS - Projeto Dominó 
+//09/08/2024 - Grupo: GGMS
+//Gabriel Azevedo Cruz 
+//Giulia Meninel Mattedi
+//Matheus Gomes Costa Castanho Giacomini
+//Sthefany Viveiros Cordeiro
+
 void fCriar(tipopeca pecas[28]) {
     int k = 0;
     
