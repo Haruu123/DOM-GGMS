d54d9db14671e7ebf0cdfc1867a8e485844e0013
gmattedi7 <giuliamema@hotmail.com>
2024-08-15 10:14:11 -0300

    Update Dom_GGMS_View.cpp

--- Dom_GGMS_View.cpp
+++ Dom_GGMS_View.cpp
@@ -5,6 +5,8 @@
 //Matheus Gomes Costa Castanho Giacomini
 //Sthefany Viveiros Cordeiro
 
+#include <stdio.h>
+
 void fMostra(tipopeca pecas[28]) {
     for (int i = 0; i < 28; i++) {
         printf("[%d|%d] ", pecas[i].lado1, pecas[i].lado2);
