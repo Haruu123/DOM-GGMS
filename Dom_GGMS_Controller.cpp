a60e8472f3d9c97b86e8207149481c586bda0bcc
gmattedi7 <giuliamema@hotmail.com>
2024-08-15 09:51:08 -0300

    Update Dom_GGMS_Controller.cpp

--- Dom_GGMS_Controller.cpp
+++ Dom_GGMS_Controller.cpp
@@ -1,3 +1,10 @@
+//DOM-GGMS - Projeto Dominó 
+//09/08/2024 - Grupo: GGMS
+//Gabriel Azevedo Cruz 
+//Giulia Meninel Mattedi
+//Matheus Gomes Costa Castanho Giacomini
+//Sthefany Viveiros Cordeiro
+
 void jogo()
 {
     fMostra();
@@ -9,8 +16,6 @@ void jogo()
     fMostra();
     fEmbaralha();
 }
-
-
 
 void fEmbaralha(tipopeca pecas[28]) {
     srand(time(NULL));
