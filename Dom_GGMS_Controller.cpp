577ee1138c7e1e323c61820abfee2078e67f90b7
gmattedi7 <giuliamema@hotmail.com>
2024-08-15 09:30:28 -0300

    Update Dom_GGMS_Controller.cpp

--- Dom_GGMS_Controller.cpp
+++ Dom_GGMS_Controller.cpp
@@ -1,1 +1,12 @@
-
+void fEmbaralha(tipopeca pecas[28]) {
+    srand(time(NULL));
+    
+    for (int i = 0; i < 28; i++) {
+        int j = rand() % 28;
+        
+        //Troca as pecas de lugar
+        tipopeca temp = pecas[i];
+        pecas[i] = pecas[j];
+        pecas[j] = temp;
+    }
+}
