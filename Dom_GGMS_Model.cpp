6a49641734efe2a27501eace55bd232a8ba7eaa8
gmattedi7 <giuliamema@hotmail.com>
2024-08-15 09:30:15 -0300

    Update Dom_GGMS_Model.cpp

--- Dom_GGMS_Model.cpp
+++ Dom_GGMS_Model.cpp
@@ -1,1 +1,11 @@
-
+void fCriar(tipopeca pecas[28]) {
+    int k = 0;
+    
+    for (int i = 0; i <= 6; i++) {
+        for (int j = i; j <= 6; j++) {
+            pecas[k].lado1 = i;    //contador atribuido pra lado1
+            pecas[k].lado2 = j;    //contador atribuido pra lado2
+            k++;
+        }
+    }
+}
