4b0ec1e65e0a16c945fb3b600fb6e317555fcab1
gmattedi7 <giuliamema@hotmail.com>
2024-08-26 18:59:54 -0300

    Update DOM_GGMS_View.cpp

--- DOM_GGMS_View.cpp
+++ DOM_GGMS_View.cpp
@@ -14,3 +14,17 @@ void fMostra(tipopeca pecas[28]) {
     }
     printf("\n\n");
 }
+
+void apresentarMenuGeral()
+{
+	int op;
+
+    printf("(1) iniciar jogo\n");
+//    printf("(2) jogada automatica\n");
+//    printf("(3) carregar jogo\n");
+//    printf("(4) sair\n");
+	printf("Escolha uma opcao: ");
+    scanf("%d", &op);
+    return op;
+}
+
