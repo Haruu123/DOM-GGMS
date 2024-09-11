1199ebc573b773e1ceca84f32741ae23a01fe637
gmattedi7 <giuliamema@hotmail.com>
2024-09-10 22:21:32 -0300

    Update DOM_GGMS_Model.cpp

--- DOM_GGMS_Model.cpp
+++ DOM_GGMS_Model.cpp
@@ -14,12 +14,12 @@ Mesa mesa;
 tipopeca pecas[28];
 tipopeca jogador1[7];
 tipopeca jogador2[7];
-tipopeca monte[14];   //Monte pecas disponiveis
+tipopeca monte[14]; // monte pecas disponiveis
 int numeroPecasNoMonte = 14;
 int numeroPecasDoJogador1 = 7;
 int numeroPecasDoJogador2 = 7;
 
-void fCriar(tipopeca pecas[]){
+void fCriar(tipopeca pecas[]) {
     int k = 0;
     for (int i = 0; i <= 6; i++) {
         for (int j = i; j <= 6; j++) {
@@ -31,29 +31,31 @@ void fCriar(tipopeca pecas[]){
     }
 }
 
-void distribuiPecas(tipopeca pecas[28]){
+void distribuiPecas(tipopeca pecas[28]) {
     
     for (int i = 0; i < 7; i++) {
         jogador1[i] = pecas[i];
-        jogador1[i].status = 1; //Status jogador 1
+        jogador1[i].status = 1; // status jogador 1
     }
     for (int i = 7; i < 14; i++) {
         jogador2[i - 7] = pecas[i];
-        jogador2[i - 7].status = 2; //Status jogador 2
+        jogador2[i - 7].status = 2; // status jogador 2
     }
     for (int i = 14; i < 28; i++) {
         monte[i - 14] = pecas[i];
-        monte[i - 14].status = 0; //Status monte
+        monte[i - 14].status = 0; // status monte
     }
     numeroPecasDoJogador1 = 7;
     numeroPecasDoJogador2 = 7;
     numeroPecasNoMonte = 14;
 }
 
-struct Jogo{
-    int qtMesaJogo;      //Quantidade de peças na mesa
-    int jogadorJogo;     //Jogador atual (1 ou 2)
-    int jogadorComp;     //Se o jogador 2 é o computador (0: não, 2: sim)
-    int mesaDJogo;       //Extremidade direita da mesa
-    int mesaEJogo;       //Extremidade esquerda da mesa
-} sitJogo;               //Variável global da situação do jogo
+struct Jogo {
+    int qtMesaJogo;      // Quantidade de peças na mesa
+    int jogadorJogo;     // Jogador atual (1 ou 2)
+    int jogadorComp;     // Se o jogador 2 é o computador (0: não, 2: sim)
+    int mesaDJogo;       // Extremidade direita da mesa
+    int mesaEJogo;       // Extremidade esquerda da mesa
+} sitJogo;               // Variável global da situação do jogo
+
+
